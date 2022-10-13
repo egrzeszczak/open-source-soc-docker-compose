6dc09ec06b4b2ed064d7caa2939177560688b07b
egrzeszczak <ernest.grzeszczak@protonmail.com>
2022-10-13 10:24:51 +0200

    build: :building_construction: added Dockerfiles for each of the nodes for future modifications
    You can now edit the Dockerfiles of the images used in the stack for future modifications

--- cleanup.sh
+++ cleanup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+# DOCKER
 sudo rm -rf ./ssl/wazuh/
 docker-compose down
 docker container prune -f
 docker volume prune -f
 docker network prune -f
 docker image prune -f
-docker image rm egrzeszczak/graylog:4.3-jre11
+
+# PRE
 docker image rm graylog/graylog:4.3-jre11
 docker image rm mongo:4.2
 docker image rm wazuh/wazuh-certs-generator:0.0.1
 docker image rm wazuh/wazuh-manager:4.3.8
 docker image rm wazuh/wazuh-indexer:4.3.8
 docker image rm wazuh/wazuh-dashboard:4.3.8
+
+# PROD
+docker image rm egrzeszczak/wazuh-indexer
+docker image rm egrzeszczak/wazuh-dashboard
+docker image rm egrzeszczak/graylog
+docker image rm egrzeszczak/graylog-mongo
