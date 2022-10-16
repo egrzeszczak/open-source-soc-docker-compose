c10161686b5e24ab4685f0199b641b12e64df8c6
egrzeszczak <ernest.grzeszczak@protonmail.com>
2022-10-16 22:37:55 +0200

    build: :building_construction: updated setup.sh, cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
+GRAY='\033[1;30m'
+NC='\033[0m' # No Color
 
-# DOCKER
-sudo rm -rf ./ssl/wazuh/
+# Compose down the stack
+echo -e "${GRAY}[setup.sh]: 1. docker-compose down${NC}"
 docker-compose down
+
+# Clean TLS
+echo -e "${GRAY}[setup.sh]: 2. Clean up TLS certificates${NC}"
+echo -e "${GRAY}[setup.sh]: 2.1. Generate TLS certificates for Wazuh Indexer, Wazuh Dashboard, Wazuh Manager${NC}"
+sudo rm -rf ./ssl/wazuh/
+
+# Clean up images
+echo -e "${GRAY}[setup.sh]: 3. Clean up docker${NC}"
 docker container prune -f
 docker volume prune -f
 docker network prune -f
 docker image prune -f
-
-# PRE
-# docker image rm graylog/graylog:4.3-jre11
-# docker image rm mongo:4.2
-# docker image rm wazuh/wazuh-certs-generator:0.0.1
-# docker image rm wazuh/wazuh-manager:4.3.8
-# docker image rm wazuh/wazuh-indexer:4.3.8
-# docker image rm wazuh/wazuh-dashboard:4.3.8
-
-# PROD
 docker image rm egrzeszczak/wazuh-indexer
 docker image rm egrzeszczak/wazuh-dashboard
+docker image rm egrzeszczak/wazuh-manager
 docker image rm egrzeszczak/graylog
 docker image rm egrzeszczak/graylog-mongo
+
+echo -e "${GRAY}[setup.sh]: Stack is cleaned up${NC}"
--- setup.sh
+++ setup.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+GRAY='\033[1;30m'
+NC='\033[0m' # No Color
 
+# Setup TLS
+echo -e "${GRAY}[setup.sh]: 1. Generate TLS certificates${NC}"
+echo -e "${GRAY}[setup.sh]: 1.1. Generate TLS certificates for Wazuh Indexer, Wazuh Dashboard, Wazuh Manager${NC}"
 docker-compose -f ./ssl/wazuh-generate.yml run --rm generator
-sudo docker-compose build
+
+# Build images
+echo -e "${GRAY}[setup.sh]: 2. Build images${NC}"
+docker-compose build
+
+# Configure files
+echo -e "${GRAY}[setup.sh]: 3. Configure files${NC}"
+chown root:root ./config/wazuh/wazuh_manager/filebeat/filebeat.yml
+chmod go-w ./config/wazuh/wazuh_manager/filebeat/filebeat.yml
+
+
+echo -e "${GRAY}[setup.sh]: Finished \n\nYou can now run${NC} docker-compose up -d ${GRAY}to run the stack${NC}\n\n"
